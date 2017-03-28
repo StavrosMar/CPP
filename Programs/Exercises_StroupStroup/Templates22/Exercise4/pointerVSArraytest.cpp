318a6d076aa0fd677b2a5936a8a815d76416f95b
StavrosMar <redenicht@hotmail.com>
2017-03-28 16:48:11 +0200

    More comprehensive example
    Added a larger variety of pointer/ array access examples

--- Programs/Exercises_StroupStroup/Templates22/Exercise4/pointerVSArraytest.cpp
+++ Programs/Exercises_StroupStroup/Templates22/Exercise4/pointerVSArraytest.cpp
@@ -1,15 +1,21 @@
+// PointerVSArray test
+
 #include <iostream>
 
-int main() { 
+int main()
+{
 
-	//int* ptr = new int[10];
-	int ptr[10];
-	ptr[0] = 12;
-	ptr[1] = 13;
-	std::cout<< "Value ptr = : "<< ptr <<std::endl;
-	std::cout<< "Value *ptr = : "<< *ptr <<std::endl;
-	std::cout<< "Value ptr[0] = :" << ptr[0] <<std::endl;
-	std::cout<< "Value ptr[1] = :" << ptr[1] <<std::endl;
-	std::cout<< "Value *(ptr+1) = :" << *(ptr+1) <<std::endl;
-	return 0;
+// stavros is actually a pointer pointing to the start of the array
+ int stavros[3]={1,2,3};
+
+// getting p to point to the same memory address as stavros - thus we can access it the same way as we do with stavros
+ int* p=stavros;
+ 
+ std::cout<< *stavros << std::endl;         //dereferencing the pointer
+ std::cout<< *(stavros + 1) << std::endl;   //getting actually stavros[0+1] (as 0 is where stavros is pointing to)
+ std::cout<< *(p + 1) << std::endl;
+ std::cout<< stavros[1] << std::endl;
+ std::cout<< p[1] << std::endl;
+ 
+ return 0;
 }
