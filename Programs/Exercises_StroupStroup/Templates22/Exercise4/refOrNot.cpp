186a14dda6aa65fc19b1a7f1fa2de348f652dccd
StavrosMar <redenicht@hotmail.com>
2017-04-06 00:23:48 +0200

    latest submission - proof

--- Programs/Exercises_StroupStroup/Templates22/Exercise4/refOrNot.cpp
+++ Programs/Exercises_StroupStroup/Templates22/Exercise4/refOrNot.cpp
@@ -1,5 +1,6 @@
 // This program is intended to find out the implications 
 // of using <type>& in return types from functions
+// ** Bottom Line : if it is not a member function of class : DISASTER!!!!!!
 
 #include<iostream>
 
@@ -15,25 +16,33 @@ public:
 
 	// Access functions
 
-	C* getVal() {  
+	C& getVal() {  
 		
-		return &value;
+		return value;
 	}
 		
 private:
 	C value;
 };
 
-const int* func1(const int* garde ) {
 
-	return garde;
+// disaster if returning reference or pointer to the value different to the one being fed.
+// if the reference is removed from int& func1 then fine.
+// If i do return garde by reference it does not crash probably because the compiler 
+// is smart enough to not actually anything inside the function
+int& func1(int& garde ) {
+
+	int nikolaos{3};
+
+	return nikolaos;
+
 
 }
 
 int main() {
 
 	Wrapper<int> maria{1};
-	Wrapper<int> nikos( *func1(maria.getVal()) );
+	Wrapper<int> nikos( func1(maria.getVal()) );
 
 	std::cout << nikos.getVal() << std::endl;
 
