667fe5a6f06024a7624902b75e5682cf7c51bd3a
StavrosMar <redenicht@hotmail.com>
2017-05-01 17:54:07 +0200

    Clean up & added Function Object implementation

--- Programs/Exercises_StroupStroup/Templates22/Exercise5/Exercise5.cpp
+++ Programs/Exercises_StroupStroup/Templates22/Exercise5/Exercise5.cpp
@@ -5,37 +5,11 @@
 #include<iostream>
 #include<iterator>
 #include<algorithm>
-
-// Class declarations
-
-// Function declarations
-
-/*
-// sort function template
-	// Option 1 - using sort from std
-template<typename T>
-void sort(std::iterator &a, std::iterator &b) {
-
-	// std::sort needs Compare type as input
-	std::sort(&a, &b, &criteria);
-}
+#include<functional>
 
 
-// print function
-
-void printVec( iterator a, iterator b) {
-	
-	for (auto i = a ; i != b ; ++i ) {
-	
-		std::cout<< "e" << *i << " ";
-	}
-	
-	std::cout<< "\n" ;
-	
-}
-*/
-
-namespace stavros {
+// namespace - sort to be used with function pointers as comp criteria
+namespace fp {
 	
 	// sort
 	template<typename T, bool (*f)(const T&, const T&)>       //!! F f instead of typename F. #Non-type template param
@@ -61,16 +35,35 @@ namespace stavros {
 
 }
 
+// namespace for sort to be used with function objects (as original sort is)
+namespace fo {
+
+
+	// Comparison function object: greater
+	template<typename T>
+	class greater {
+		
+	public:
+	
+		bool operator() (const T &a,const T &b) {
+		
+			return (a > b) ? true : false;
+		}
+	};
+
+}
 
 // main 
 int main() {
 
 	std::vector<int> vec0{1,2,3};
-	
-	//std::cout<<stavros::greater<int>(1,2)<<std::endl;;
-	stavros::sort<int,stavros::greater<int>>;
 
-//	sort(vec0.begin(), vec0.end());
+	// Testing funPointer fp:sort
+	fp::sort<int,fp::greater<int>>;
+
+	//Testing funObject fo:sort
+	std::array<int, 10> s = {5, 7, 4, 2, 8, 6, 1, 9, 0, 3}; 
+	std::sort(s.begin(),s.end(),fo::greater<int>());
 /*
 	std::cout<< "The sorted vector is the following : " << std::endl;
 	printVec( vec0.begin(), vec0.end() )<< std::endl;
