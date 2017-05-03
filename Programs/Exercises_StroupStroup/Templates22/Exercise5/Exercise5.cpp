2ceb10e035f0d5a88eaf184b0bdf1c8cd96537f2
StavrosMar <redenicht@hotmail.com>
2017-05-03 23:06:55 +0200

    fo::sort (function objects) complete

--- Programs/Exercises_StroupStroup/Templates22/Exercise5/Exercise5.cpp
+++ Programs/Exercises_StroupStroup/Templates22/Exercise5/Exercise5.cpp
@@ -40,32 +40,67 @@ namespace fo {
 
 
 	// Comparison function object: greater
+
 	template<typename T>
 	class greater {
 		
 	public:
 	
-		bool operator() (const T &a,const T &b) {
+		bool operator() (const T& a,const T &b) {
 		
 			return (a > b) ? true : false;
 		}
 	};
 
+	// Sort Algorithm 
+	template<typename T, typename RIter>		// RIter deducted directly from args
+	void sort(const RIter &a, const RIter &b) {
+
+		std::sort(a, b, T());
+
+	}
+
+
 }
+
+// Very important to use & in the return -  ostream is non-copyable
+template<typename C>
+std::ostream& operator<<(std::ostream& os, const std::vector<C> &vec) {
+
+	for ( auto x : vec ) {
+
+		os<<x<<" ";
+	}
+
+	return os;
+
+}
+
+		
 
 // main 
 int main() {
-
-	std::vector<int> vec0{1,2,3};
 
 	// Testing funPointer fp:sort
 	fp::sort<int,fp::greater<int>>;
 
 	//Testing funObject fo:sort
-	std::array<int, 10> s = {5, 7, 4, 2, 8, 6, 1, 9, 0, 3}; 
+	std::vector<int> s{5, 7, 4, 2, 8, 6, 1, 9, 0, 3}; 
+	
+	//-//Testing fo::greater with std::sort
 	std::sort(s.begin(),s.end(),fo::greater<int>());
-/*
-	std::cout<< "The sorted vector is the following : " << std::endl;
-	printVec( vec0.begin(), vec0.end() )<< std::endl;
-*/	
+
+	 //-//Testing fo::sort with std::greater
+	
+	fo::sort<std::greater<int>>(s.begin(),s.end());
+	
+	std::cout<<s<<std::endl;
+	
+	//-//Testing fo::sort with fo::greater
+	
+	std::vector<int> vecFOFull{10,4,5,100,200,2};
+	
+	fo::sort<fo::greater<int>>(vecFOFull.begin(),vecFOFull.end());
+	
+	std::cout<<vecFOFull<<std::endl;
 }
