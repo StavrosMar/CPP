61177390da0f89de009a2a89207d770bf84cb59f
Stavros Marinis <redenicht@hotmail. com>
2017-05-04 22:18:53 +0200

    Function to use sort on Record

---
+++ Programs/Exercises_StroupStroup/Templates22/Exercise5/Exerc5-prototypeRecord.cpp
@@ -0,0 +1,25 @@
+// Exercise 5b - Record and sort on count and price 
+//This is a prototype fpr 
+
+#include <iostream>
+
+int main() {
+
+// sort as we know it / syntax familiar
+
+vector<int> svec{19,23,56,77};
+sort<std::greater<int>>(svec.begin,svec.end);
+
+template<typename T>
+class Record {
+
+public :
+
+	T count;
+	T price;
+	
+}
+
+return 0
+
+
