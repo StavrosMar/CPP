237521cb9bc4fcec6f9cebb1ef8a9f68506583dd
Stavros Marinis <redenicht@hotmail. com>
2017-05-08 14:35:35 +0100

    Updates - flybe

--- Programs/Exercises_StroupStroup/Templates22/Exercise5/Exerc5-prototypeRecord.cpp
+++ Programs/Exercises_StroupStroup/Templates22/Exercise5/Exerc5-prototypeRecord.cpp
@@ -3,23 +3,54 @@
 
 #include <iostream>
 
-int main() {
-
-// sort as we know it / syntax familiar
-
-vector<int> svec{19,23,56,77};
-sort<std::greater<int>>(svec.begin,svec.end);
-
 template<typename T>
 class Record {
 
 public :
 
+	// Constructor 
+	Record(T& c, T& p) : count{c}, price{p} {};
+	
+	
+	
+	// public members
 	T count;
 	T price;
 	
+};
+
+// Comparison wrapper - generic use for the following:
+//-// T is for the objects
+//-// Comp is for the function object used for the comparison
+//-// C is for the object member i want to sort on
+
+template<typename T,typename Comp, typename C>
+bool compwrapper(const& T obj1, const& T obj2) {
+	
+	//how the hell am i going to pass the member ? pointer maybe?
+	C memsort;
+	
+	//execute operator on the member 
+	obj1.memsortComp()
+	
 }
 
-return 0
+
+
+int main() {
+
+// sort as we know it / syntax familiar
+
+vector<int> svec{19,23,56,77};
+sort<std::greater<int>>(svec.begin(),svec.end());
+
+// Sort on Record data memebers
+
+std::vector<Record<int>> vecRec{Record<int>(10,1), Record<int>(4,100)};
+
+fo::sort<std::greater<Record<int>>()>(vecRec.begin(),vecRec.end());
+
+return 0;
+
 
 
