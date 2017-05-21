5b1a0dce542299c043647a263c418dfab3384d46
StavrosMar <redenicht@hotmail.com>
2017-05-21 16:28:44 +0200

    TestMap

---
+++ Programs/Exercises_StroupStroup/Templates22/Exercise8/Exercise8.cpp
@@ -0,0 +1,46 @@
+//Templates 23 - Exercise 8
+// Create a Map Class
+
+//<Details>
+
+
+
+#include <iostream>
+#include <algorithm>
+#include <map>
+
+namespace stavros {
+
+	//* functions defined here *//
+
+} 
+
+int main() {
+
+
+// original map function - test with duplicated keys
+std::map<std::string,int> maptic{
+	
+	{"Anna-Liz", 0},
+	{"Delphine", 1},
+	{"Delphine", 2}
+};
+
+// It is printing the first result
+std::cout<<maptic["Delphine"]<<std::endl;
+	
+//TEST1 - Sorting Matrices
+	
+	//-//Set-up
+	
+	
+	//-//Calling point
+
+
+	//-//Print results
+	
+//TEST1 - END
+
+	return 0;
+
+}
