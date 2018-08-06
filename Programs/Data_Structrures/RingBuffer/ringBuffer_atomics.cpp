8328289a17af0200213185116f0fbcffbda5650b
Stavros Marinis <redenicht@hotmail.com>
2018-08-06 17:48:40 +0200

    Stripping down - keeping only necessary

--- Programs/Data_Structrures/RingBuffer/ringBuffer_atomics.cpp
+++ Programs/Data_Structrures/RingBuffer/ringBuffer_atomics.cpp
@@ -1,8 +1,8 @@
 /*
+*               Stripped down implementation of ringBuffer.cpp 
 *
-*   Topics covered:
-*                1) Construct a Ring Buffer
-*                2) TODO - lock free
+*   Topics covered :
+*                1) TODO - lock free
 *                3) TODO - define iterators
 *
 */
@@ -16,7 +16,6 @@ template <class T> class RingBuffer {
 
 private:
     const unsigned int N;
-    unsigned int prevN; //previous size : Needed for copy constructor
     T* A{nullptr}; // array
     unsigned int head;
     unsigned int tail;
@@ -28,62 +27,22 @@ private:
     
 public:
     
-    RingBuffer(const unsigned int& iN) :N{iN}, A{new T[N]}, head{0},tail{0} { prevN = N; };
-    
+    //Constructors - Destructor
+    RingBuffer(const unsigned int& iN) :N{iN}, A{new T[N]}, head{0},tail{0} {};
     RingBuffer() = delete;
-    
-    //Copy constructor
-    RingBuffer(const RingBuffer& r) :  N{r.N} { 
-        
-        if (this != nullptr && this->prevN!=r.N ) { //Manage resources
-            delete[] A;
-            this->A = new T[r.N];
-            prevN = N;
-        }
-        
-        if ( this != &r) { //Self Assignment;
-        
-            this->head = r.head;
-            this->tail = r.tail;
-            this->count = r.count;
-        
-            for (int i{0}; i<N; ++i) {
-                this->A[i] = r.A[i];
-            }   
-        }
-    };
-    
+    RingBuffer(const RingBuffer& r) = delete;
     ~RingBuffer() { delete[] A;}
     
     //Public interface
-    
     void push(const T& s); //Add smth in the buffer
-    
     void pop(); //Remove last elem from the buffer
-    
-    T front();  //Take head from buffer
-    T back();   //Take tail from buffer
-    
-    bool empty();
     unsigned int capacity() const;
-    
-    //STL interface for range-loops
-    const_iter begin();
-    const_iter end();
 
 };
 
 template <typename T> unsigned int RingBuffer<T>::capacity() const {
     return count;
 };
-
-template <typename T> T  RingBuffer<T>::front() {
-    return A[head];
-}
-
-template <typename T> T  RingBuffer<T>::back() {
-    return A[tail];
-}
 
 template <typename T> void RingBuffer<T>::push(const T& s) {
     
@@ -109,7 +68,7 @@ int main() {
     
     const int buffSize{4};
     RingBuffer<int> buff1(buffSize);
-    RingBuffer<int> buff2(buff1);
+    //RingBuffer<int> buff2(buff1);
   
     buff1.push(1);
     buff1.push(4);
@@ -123,3 +82,4 @@ int main() {
    // for (auto elem : buff1) {
     //    cout<<"Elem"<<elem<<'\n';
     //}
+}
